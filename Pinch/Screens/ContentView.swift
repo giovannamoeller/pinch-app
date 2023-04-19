2a7be9324ff9e1290954d267954d1a6a4fa0c594
giovannamoeller <giovannagmoeller@gmail.com>
2023-04-19 16:48:36 -0300

    adding image into view

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundColor(.accentColor)
-      Text("Hello, world!")
+    NavigationView {
+      ZStack {
+        Image("magazine-front-cover")
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .cornerRadius(10)
+          .padding()
+          .shadow(color: .black.opacity(0.4), radius: 12, x: 2, y: 2)
+      }
+      .navigationTitle("Pinch and Zoom")
+      .navigationBarTitleDisplayMode(.inline)
     }
-    .padding()
   }
 }
 
