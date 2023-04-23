286bc3b79aee18c4dbb848f8fe62d8256df1c4fb
giovannamoeller <giovannagmoeller@gmail.com>
2023-04-23 01:01:07 -0300

    creating page model

---
+++ Pinch/Models/PageModel.swift
@@ -0,0 +1,15 @@
+//
+//  PageModel.swift
+//  Pinch
+//
+//  Created by Giovanna Moeller on 23/04/23.
+//
+
+import Foundation
+
+struct Page: Identifiable {
+  let id: Int
+  let imageName: String
+}
+
+
